03dfaf45bda7db664d37ed22d55c2e8ec1d6f540
memenfor <101852008+memenfor@users.noreply.github.com>
2022-10-26 16:43:32 -0400

    Update DemoApplication.java

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        String name = "queenie";
+
+        System.out.println(name);
+    }
 
 }
+
